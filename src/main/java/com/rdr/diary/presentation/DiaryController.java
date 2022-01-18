ca89981e23f8aba3d5a661d0c0e83f4fc3ccfbcc
leeheechan <leeheefull@gmail.com>
2022-01-18 18:37:35 +0900

    feat: swagger 추가

--- src/main/java/com/rdr/diary/presentation/DiaryController.java
+++ src/main/java/com/rdr/diary/presentation/DiaryController.java
@@ -1,4 +1,4 @@
-package com.rdr.diary.presentaion;
+package com.rdr.diary.presentation;
 
 import com.rdr.common.dto.BaseResponse;
 import com.rdr.common.dto.ResponseMessage;
@@ -6,17 +6,21 @@ import com.rdr.diary.application.DiaryService;
 import com.rdr.diary.converter.DiaryConverter;
 import com.rdr.diary.dto.*;
 import com.rdr.diary.dto.bundle.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
+@Api("Diary")
 @RequestMapping("/v1/diaries")
 @RestController
 public class DiaryController {
     private final DiaryService diaryService;
     private final DiaryConverter diaryConverter;
 
+    @ApiOperation("Diary 생성")
     @PostMapping
     public ResponseEntity<BaseResponse<DiaryCreateDto.Response>> create(@RequestBody DiaryCreateDto.Request request) {
         DiaryCreateBundle.Request bundleRequest = diaryConverter.toDiaryCreateBundleRequest(request);
@@ -26,6 +30,7 @@ public class DiaryController {
         return BaseResponse.of(ResponseMessage.DIARY_CREATE_SUCCESS, response);
     }
 
+    @ApiOperation("Diary 전체 찾기")
     @GetMapping
     public ResponseEntity<BaseResponse<DiaryFindAllDto.Response>> find() {
         DiaryGetCountBundle.Response countBundleResponse = diaryService.getCount();
@@ -35,6 +40,7 @@ public class DiaryController {
         return BaseResponse.of(ResponseMessage.DIARIES_FIND_SUCCESS, response);
     }
 
+    @ApiOperation("Diary 하나 찾기")
     @GetMapping("/{id}")
     public ResponseEntity<BaseResponse<DiaryFindDto.Response>> find(@PathVariable Long id) {
         DiaryFindBundle.Request bundleRequest = DiaryFindBundle.Request.of(id);
@@ -44,6 +50,7 @@ public class DiaryController {
         return BaseResponse.of(ResponseMessage.DIARY_FIND_SUCCESS, response);
     }
 
+    @ApiOperation("Diary 수정")
     @PutMapping("/{id}")
     public ResponseEntity<BaseResponse<DiaryUpdateDto.Response>> update(@PathVariable Long id, @RequestBody DiaryUpdateDto.Request request) {
         DiaryUpdateBundle.Request bundleRequest = diaryConverter.toDiaryUpdateBundleRequest(id, request);
@@ -53,6 +60,7 @@ public class DiaryController {
         return BaseResponse.of(ResponseMessage.DIARY_UPDATE_SUCCESS, response);
     }
 
+    @ApiOperation("Diary 상태 변경")
     @PatchMapping("/{id}")
     public ResponseEntity<BaseResponse<DiaryUpdateStatusDto.Response>> updateStatus(@PathVariable Long id) {
         DiaryUpdateStatusBundle.Request bundleRequest = DiaryUpdateStatusBundle.Request.of(id);
@@ -62,6 +70,7 @@ public class DiaryController {
         return BaseResponse.of(ResponseMessage.DIARY_UPDATE_STATUS_SUCCESS, response);
     }
 
+    @ApiOperation("Diary 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<BaseResponse<DiaryDeleteDto.Response>> delete(@PathVariable Long id) {
         DiaryDeleteBundle.Request bundleRequest = DiaryDeleteBundle.Request.of(id);
